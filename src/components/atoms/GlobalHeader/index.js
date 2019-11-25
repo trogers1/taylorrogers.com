65eb13fc930dbfedb5e545b68df1d75b4a98ea16
Taylor Rogers <taylor.rogers71@gmail.com>
2019-11-25 08:27:38 -0800

    trying new colors

--- src/components/atoms/GlobalHeader/index.js
+++ src/components/atoms/GlobalHeader/index.js
@@ -2,30 +2,27 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-import { pink, yellow } from 'helpers/colors';
+import { mutedYellow } from 'helpers/colors';
 
 const HeaderText = styled(Link)`
-  color: ${yellow};
-  font-size: 4rem;
+  color: ${mutedYellow};
+  font-size: 3rem;
   font-weight: 900;
   font-style: italic;
   letter-spacing: 0.25rem;
   margin-right: 1rem;
   text-decoration: none;
   @media screen and (min-width: 800px) {
-    font-size: 4rem;
     margin-left: 0.5rem;
-    margin-top: 1.5rem;
   }
 `;
 
 const StyledDiv = styled.header`
   align-items: center;
-  background: ${pink};
   box-sizing: border-box;
   display: flex;
   font-size: 1.2rem;
-  height: 4.5rem;
+  height: 3rem;
   left: 0;
   justify-content: flex-end;
   top: 0;
--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -1,8 +1,12 @@
 // colors
 export const cardBackground = 'rgba(3, 3, 3, 0.48)';
 export const grayDark = '#6C6D6E';
+export const mutedBlue = '#237385';
+export const mutedOrange = '#C56651';
+export const mutedYellow = '#C87D20';
 export const pink = '#F0547D';
 export const purple = '#2F0743';
+export const siteBackground = '#25292D'; // Find in index.css
 export const yellow = '#F0BA47';
 
 // misc
@@ -10,9 +14,13 @@ export const gradient = 'radial-gradient(529.00px at 50% 50%, #776082 0%, #2F074
 
 export default {
   cardBackground,
+  gradient,
   grayDark,
+  mutedBlue,
+  mutedOrange,
+  mutedYellow,
   pink,
   purple,
-  yellow,
-  gradient
+  siteBackground,
+  yellow
 };
