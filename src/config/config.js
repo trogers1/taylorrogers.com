393af30dda8de999341b1d667a169ae150a10431
Taylor Rogers <taylor.rogers71@gmail.com>
2019-11-26 09:04:44 -0800

    Add config

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+const { REACT_APP_GOOGLE_ANALYTICS_TRACKING_NUMBER } = process.env;
+
+const config = {
+  googleAnalyticsTrackingNumber: REACT_APP_GOOGLE_ANALYTICS_TRACKING_NUMBER
+};
+
+export default config;
