aa560a63ed7625f19ac8a6066f78562bc26ed000
Taylor Rogers <taylor.rogers71@gmail.com>
2019-12-19 10:19:17 -0800

    Fix header linking

--- src/components/atoms/MdHeader/index.js
+++ src/components/atoms/MdHeader/index.js
@@ -14,12 +14,42 @@ const StyledLink = styled(Link)`
 
 export default ({ children, id, location, headerType }) => {
   return (
-    <StyledLink id={id} to={`${location.pathname}#${id}`}>
-      {headerType === 'h2' ? <h2 style={{ display: 'inline-block' }}>{children}</h2> : null}
-      {headerType === 'h3' ? <h3 style={{ display: 'inline-block' }}>{children}</h3> : null}
-      {headerType === 'h4' ? <h4 style={{ display: 'inline-block' }}>{children}</h4> : null}
-      {headerType === 'h5' ? <h5 style={{ display: 'inline-block' }}>{children}</h5> : null}
-      {headerType === 'h6' ? <h6 style={{ display: 'inline-block' }}>{children}</h6> : null}
-    </StyledLink>
+    <>
+      {headerType === 'h2' ? (
+        <h2>
+          <StyledLink id={id} to={`${location.pathname}#${id}`}>
+            {children}
+          </StyledLink>
+        </h2>
+      ) : null}
+      {headerType === 'h3' ? (
+        <h3>
+          <StyledLink id={id} to={`${location.pathname}#${id}`}>
+            {children}
+          </StyledLink>
+        </h3>
+      ) : null}
+      {headerType === 'h4' ? (
+        <h4>
+          <StyledLink id={id} to={`${location.pathname}#${id}`}>
+            {children}
+          </StyledLink>
+        </h4>
+      ) : null}
+      {headerType === 'h5' ? (
+        <h5>
+          <StyledLink id={id} to={`${location.pathname}#${id}`}>
+            {children}
+          </StyledLink>
+        </h5>
+      ) : null}
+      {headerType === 'h6' ? (
+        <h6>
+          <StyledLink id={id} to={`${location.pathname}#${id}`}>
+            {children}
+          </StyledLink>
+        </h6>
+      ) : null}
+    </>
   );
 };
