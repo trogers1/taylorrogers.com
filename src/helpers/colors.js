dde978ba37d6dda0de0296ebb2dfb1bb24b87c7a
Taylor Rogers <taylor.rogers71@gmail.com>
2019-12-19 07:49:49 -0800

    Add new colors

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -1,11 +1,16 @@
 // colors
-export const cardBackground = 'rgba(3, 3, 3, 0.48)';
+export const cardBackground = 'rgba(3, 3, 3, 0.25)';
+export const codeBackgroundDarkBlue = '#141c27';
+export const codeBorderMutedPurple = '#263449';
 export const grayDark = '#6C6D6E';
 export const mutedBlue = '#237385';
+export const mutedDarkBlue = '#232D34';
 export const mutedOrange = '#C56651';
 export const mutedYellow = '#C87D20';
+export const mutedPink = '#b97195';
 export const pink = '#F0547D';
 export const purple = '#2F0743';
+export const textGrey = '#b9b9b9';
 export const siteBackground = '#25292D'; // Find in index.css
 export const yellow = '#F0BA47';
 
@@ -14,13 +19,18 @@ export const gradient = 'radial-gradient(529.00px at 50% 50%, #776082 0%, #2F074
 
 export default {
   cardBackground,
+  codeBackgroundDarkBlue,
+  codeBorderMutedPurple,
   gradient,
   grayDark,
   mutedBlue,
+  mutedDarkBlue,
   mutedOrange,
   mutedYellow,
+  mutedPink,
   pink,
   purple,
+  textGrey,
   siteBackground,
   yellow
 };
