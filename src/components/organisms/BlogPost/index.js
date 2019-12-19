6205ed189cfab4af6ea30db6bef41e13d3b81c1d
Taylor Rogers <taylor.rogers71@gmail.com>
2019-12-19 07:59:26 -0800

    Update BlogPost to display markdown, scroll to hash

--- src/components/organisms/BlogPost/index.js
+++ src/components/organisms/BlogPost/index.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Markdown from 'markdown-to-jsx';
+
+import config from 'config/config';
+import getOverrides from 'helpers/mdComponentOverrides';
 
 import HeadingLarge from 'components/atoms/HeadingLarge';
 import LargeCard from 'components/atoms/LargeCard';
@@ -10,23 +14,43 @@ const Centered = styled.div`
   flex-direction: column;
   justify-content: center;
   min-height: 100%;
+  width: 100%;
 `;
 
 const BlogPage = ({ location }) => {
-  const [article, setArticle] = useState(null);
+  const overrides = getOverrides(location);
+  const [article, setArticle] = useState('');
+  let blogId = location.pathname;
+  blogId = blogId.split('/')[2];
   useEffect(() => {
     async function getArticle() {
-      fetch('/Users/admin/Documents/Code/taylor-rogers/public/posts/Start-Notes.md')
-        .then(res => res.text())
-        .then(text => setArticle(text));
+      let res = await fetch(`${config.blogApiUrl}/blog/${blogId}?fullText=true`, { method: 'GET' });
+      res = await res.json();
+      setArticle(res.data.attributes);
     }
     getArticle();
   }, []);
 
+  // Scroll to hash, if present
+  useEffect(() => {
+    const { hash } = location;
+    const id = hash.replace(/#/g, '');
+    if (hash && document.getElementById(id)) {
+      const scrollOffset = -60;
+      const element = document.getElementById(id);
+      const scrollTo = element.getBoundingClientRect().top + window.pageYOffset + scrollOffset;
+      window.scrollTo({ top: scrollTo, behavior: 'smooth' });
+    }
+  }, [location.hash, article]); // Fires every time hash changes or article is loaded
+
   return (
     <Centered>
-      <HeadingLarge>Welcome</HeadingLarge>
-      <LargeCard>{article}</LargeCard>
+      <HeadingLarge>{article ? article.title : 'Loading...'}</HeadingLarge>
+      {article && (
+        <LargeCard>
+          <Markdown options={{ overrides }}>{article.body}</Markdown>
+        </LargeCard>
+      )}
     </Centered>
   );
 };
