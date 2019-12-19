746bd49d7e16ef4620fae31c4853afd88be7c743
Taylor Rogers <taylor.rogers71@gmail.com>
2019-12-19 07:49:34 -0800

    Get blog api url per environment

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,10 @@
-const { REACT_APP_GOOGLE_ANALYTICS_TRACKING_NUMBER } = process.env;
+const { REACT_APP_GOOGLE_ANALYTICS_TRACKING_NUMBER, REACT_APP_ENV } = process.env;
+const REACT_APP_BLOG_POST_API =
+  process.env[`REACT_APP_BLOG_POST_API_${REACT_APP_ENV === 'PROD' ? 'PROD' : 'QA'}`];
 
 const config = {
+  blogApiUrl: REACT_APP_BLOG_POST_API,
+  env: REACT_APP_ENV,
   googleAnalyticsTrackingNumber: REACT_APP_GOOGLE_ANALYTICS_TRACKING_NUMBER
 };
 
