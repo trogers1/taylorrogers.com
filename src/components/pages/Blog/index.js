232be95771af967ad176ed919591db32bce0e47d
Taylor Rogers <taylor.rogers71@gmail.com>
2019-12-19 08:01:04 -0800

    start on setting valid blog posts for /blog

--- src/components/pages/Blog/index.js
+++ src/components/pages/Blog/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import config from 'config/config';
 
 import BlogHome from 'components/organisms/BlogHome';
 import BlogPost from 'components/organisms/BlogPost';
@@ -7,18 +8,24 @@ import BlogPost from 'components/organisms/BlogPost';
 const Blog = ({ location }) => {
   const [validBlogPosts, setValidBlogPosts] = useState([]);
   useEffect(() => {
-    setValidBlogPosts([
-      {
-        title: 'blah'
-      }
-    ]);
+    async function getArticles() {
+      let res = await fetch(`${config.blogApiUrl}/blog/`, { method: 'GET' });
+      res = await res.json();
+      setValidBlogPosts(res.data);
+    }
+    getArticles();
   }, []);
   return (
     <Switch>
       <Route path="/blog" exact component={BlogHome} />
       <Route key={`blogPost-kong`} path={`/blog/kong`} exact component={BlogPost} />
-      {validBlogPosts.map((blogPost, index) => (
-        <Route key={`blogPost-${index}`} path={`/blog/${blogPost.id}`} exact component={BlogPost} />
+      {validBlogPosts.map(blogPost => (
+        <Route
+          key={`blogPost-${blogPost.attributes.title}`}
+          path={`/blog/${blogPost.id}`}
+          exact
+          component={BlogPost}
+        />
       ))}
       <Redirect to="/" />
     </Switch>
