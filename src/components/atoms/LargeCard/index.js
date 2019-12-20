935f34bf434271c6504e0cae82635fc019609a28
Taylor Rogers <taylor.rogers71@gmail.com>
2019-12-20 10:21:16 -0800

    make LargeCard responsive

--- src/components/atoms/LargeCard/index.js
+++ src/components/atoms/LargeCard/index.js
@@ -4,16 +4,21 @@ import { cardBackground, textGrey } from 'helpers/colors';
 
 export default styled.div`
   background: ${cardBackground};
+  box-sizing: border-box;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   color: ${textGrey};
-  display: flex;
-  flex-direction: column;
   font-size: 1.6rem;
   height: auto;
   margin: 2rem;
   margin-bottom: 5rem;
   max-width: 90%;
-  padding: 2rem;
+  padding: 1rem;
   text-decoration: none;
   width: auto;
+  word-break: break-word;
+
+  @media (max-width: 799px) {
+    max-width: 100%;
+    margin: 2rem 0.5rem;
+  }
 `;
