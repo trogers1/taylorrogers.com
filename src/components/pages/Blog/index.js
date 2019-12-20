f6fd8d1463d4b97c30797368876563b6a67c8ae2
Taylor Rogers <taylor.rogers71@gmail.com>
2019-12-20 08:54:37 -0800

    Remove unused import

--- src/components/pages/Blog/index.js
+++ src/components/pages/Blog/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import config from 'config/config';
 
 import BlogHome from 'components/organisms/BlogHome';
