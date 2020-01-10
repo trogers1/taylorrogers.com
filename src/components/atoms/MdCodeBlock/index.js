7c912dcbc6d37b9a0318838bd41f8566133757ae
Taylor Rogers <taylor.rogers71@gmail.com>
2020-01-10 11:13:46 -0800

    Update MdCodeBlock to use CodeHighlight

--- src/components/atoms/MdCodeBlock/index.js
+++ src/components/atoms/MdCodeBlock/index.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import style from 'react-syntax-highlighter/dist/esm/styles/prism/duotone-space';
 
-const StyledCode = styled.code`
-  white-space: pre;
-  word-break: break-word;
-  overflow-wrap: break-word;
-`;
+import CodeHighlight from 'components/atoms/CodeHighlight';
 
 export default ({ children }) => {
   if (children.props) {
     return (
-      <SyntaxHighlighter
+      <CodeHighlight
         language={children.props.className ? children.props.className.replace('lang-', '') : ''}
-        style={style}
-        showLineNumbers={false}
-        CodeTag={StyledCode}
       >
         {children.props.children}
-      </SyntaxHighlighter>
+      </CodeHighlight>
     );
   } else {
     return children;
