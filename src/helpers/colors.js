794c63b3801ac767f605d764727480074cfc3749
Taylor Rogers <taylor.rogers71@gmail.com>
2020-01-15 10:38:08 -0800

    Add comments to colors to figure out what is used and what isn't and what for

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -2,36 +2,26 @@
 export const cardBackground = 'rgba(3, 3, 3, 0.25)';
 export const codeBackgroundDarkBlue = '#141c27';
 export const codeBorderMutedPurple = '#263449';
-export const grayDark = '#6C6D6E';
-export const mutedBlue = '#237385';
-export const mutedDarkBlue = '#232D34';
-export const mutedOrange = '#C56651';
-export const mutedOrangeRGB = '197,102,81';
-export const mutedYellow = '#C87D20';
-export const mutedPink = '#b97195';
-export const pink = '#F0547D';
-export const purple = '#2F0743';
-export const textGrey = '#b9b9b9';
+export const mutedBlue = '#237385'; // External Links, HeadingLarge bottom border, Internal Links, a circle button
+export const mutedDarkBlue = '#232D34'; // MdBlockQuote background
+export const mutedOrange = '#C56651'; // HeadingLarge, CircleButton default, Header
+export const mutedOrangeRGB = '197,102,81'; // Markdown HR
+export const mutedYellow = '#C87D20'; // Global Header/Footer, Inline Code text color, Small Card Link Title
+export const mutedPink = '#b97195'; // Block quote left side bar, a circle button
+export const textGrey = '#b9b9b9'; // Block text
 export const siteBackground = '#25292D'; // Find in index.css
-export const yellow = '#F0BA47';
-
-// misc
-export const gradient = 'radial-gradient(529.00px at 50% 50%, #776082 0%, #2F0743 100%);';
+export const yellow = '#F0BA47'; // Circle button text
 
 export default {
   cardBackground,
   codeBackgroundDarkBlue,
   codeBorderMutedPurple,
-  gradient,
-  grayDark,
   mutedBlue,
   mutedDarkBlue,
   mutedOrange,
   mutedOrangeRGB,
   mutedYellow,
   mutedPink,
-  pink,
-  purple,
   textGrey,
   siteBackground,
   yellow
