65aaa876cf1b3a45b33baaa0d9830af22277a666
Taylor Rogers <taylor.rogers71@gmail.com>
2020-01-23 15:26:38 -0800

    add some RGB colors

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -3,10 +3,12 @@ export const cardBackground = 'rgba(3, 3, 3, 0.25)';
 export const codeBackgroundDarkBlue = '#141c27';
 export const codeBorderMutedPurple = '#263449';
 export const mutedBlue = '#237385'; // External Links, HeadingLarge bottom border, Internal Links, a circle button
+export const mutedBlueRGB = '35, 115, 133'; // About Page Skill Bar Chart
 export const mutedDarkBlue = '#232D34'; // MdBlockQuote background
 export const mutedOrange = '#C56651'; // HeadingLarge, CircleButton default, Header
-export const mutedOrangeRGB = '197,102,81'; // Markdown HR
+export const mutedOrangeRGB = '197, 102, 81'; // Markdown HR
 export const mutedYellow = '#C87D20'; // Global Header/Footer, Inline Code text color, Small Card Link Title
+export const mutedYellowRGB = '200, 125, 32'; // About Page Skill Bar Chart: hover
 export const mutedPink = '#b97195'; // Block quote left side bar, a circle button
 export const textGrey = '#b9b9b9'; // Block text
 export const siteBackground = '#25292D'; // Find in index.css
@@ -17,10 +19,12 @@ export default {
   codeBackgroundDarkBlue,
   codeBorderMutedPurple,
   mutedBlue,
+  mutedBlueRGB,
   mutedDarkBlue,
   mutedOrange,
   mutedOrangeRGB,
   mutedYellow,
+  mutedYellowRGB,
   mutedPink,
   textGrey,
   siteBackground,
