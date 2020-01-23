d558b394af7dda0629ed02e77898418392830290
Taylor Rogers <taylor.rogers71@gmail.com>
2020-01-23 15:36:52 -0800

    AboutSkills helper

---
+++ src/helpers/aboutSkills.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'React',
+    score: 85,
+    types: ['backend', 'frontend', 'devops', 'soft', 'data']
+  },
+  {
+    name: 'Javascript',
+    score: 95,
+    types: ['backend', 'frontend', 'devops', 'soft', 'data']
+  },
+  {
+    name: 'NodeJS',
+    score: 93,
+    types: ['backend', 'frontend', 'devops', 'soft', 'data']
+  },
+  {
+    name: 'Big Data',
+    score: 70,
+    types: ['backend', 'frontend', 'devops', 'soft', 'data']
+  },
+  {
+    name: 'Leadership',
+    score: 65,
+    types: ['backend', 'frontend', 'devops', 'soft', 'data']
+  },
+  {
+    name: 'Bash',
+    score: 50,
+    types: ['backend', 'frontend', 'devops', 'soft', 'data']
+  }
+];
