c9e7eb75cb2e4bc4f6965d5fe3c9657c5bf0389c
Taylor Rogers <taylor.rogers71@gmail.com>
2020-01-31 22:04:53 -0800

    StyledHr: sort

--- src/components/atoms/StyledHr/index.js
+++ src/components/atoms/StyledHr/index.js
@@ -3,14 +3,13 @@ import styled from 'styled-components';
 import { mutedOrangeRGB } from 'helpers/colors';
 
 export default styled.hr`
-  border: 0;
-  height: 0.3rem;
   background-image: linear-gradient(
     to right,
     rgba(0, 0, 0, 0),
     rgba(${mutedOrangeRGB}, 0.75),
     rgba(0, 0, 0, 0)
   );
-
+  border: 0;
+  height: 0.3rem;
   width: 75%;
 `;
