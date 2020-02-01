b6d1f7bcb8f52a6b4172791655c1caa644984233
Taylor Rogers <taylor.rogers71@gmail.com>
2020-01-31 21:39:40 -0800

    Finish About Page (big one)

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -8,6 +8,9 @@ import slugify from 'helpers/slugify';
 import Face from 'images/face.jpg';
 
 import CircleImage from 'components/atoms/CircleImage';
+import ExternalLink from 'components/atoms/ExternalLink';
+import Input from 'components/atoms/Input';
+import InternalLink from 'components/atoms/InternalLink';
 import Select from 'components/atoms/Select';
 import Header from 'components/atoms/Header';
 import TabButton from 'components/atoms/TabButton';
@@ -65,10 +68,48 @@ const StyledHeader = styled(Header)`
   width: 100%;
 `;
 
+const Flex = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: flex-start;
+  margin-left: 15%;
+  margin-right: 15%;
+
+  @media screen and (max-width: 799px) {
+    flex-direction: column;
+  }
+`;
+
+const StyledSelect = styled(Select)`
+  margin: 0.5rem 1rem;
+  max-width: 20rem;
+  min-width: 10rem;
+
+  @media screen and (max-width: 799px) {
+    max-width: unset;
+    min-width: unset;
+    width: 100%;
+  }
+`;
+
+const StyledInput = styled(Input)`
+  margin: 0.5rem 1rem;
+  max-width: 30rem;
+  min-width: 10rem;
+
+  @media screen and (max-width: 799px) {
+    max-width: unset;
+    min-width: unset;
+    width: 100%;
+  }
+`;
+
 const AboutPage = ({ location }) => {
   const [selectedTab, setSelectedTab] = useState('Developer');
+
+  // Filter variables
   const [currFilter, setFilter] = useState('Default');
-  const [displayedSkills, setDisplayedSkills] = useState([]);
+  const [filteredSkills, setFilteredSkills] = useState([]);
   const filterOptions = [
     'Default',
     'Frontend',
@@ -80,15 +121,29 @@ const AboutPage = ({ location }) => {
     'All'
   ];
 
+  // Search variables
+  const [searchInput, setSearchInput] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
   useEffect(() => {
-    if (currFilter === 'All') {
-      setDisplayedSkills(aboutSkills);
+    if (searchInput) {
+      setFilter('All');
+      setSearchResults(
+        aboutSkills
+          .filter(skill => skill.name.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase()))
+          .sort((a, b) => b.score - a.score)
+      );
+    } else if (currFilter === 'All') {
+      const sortedSkills = aboutSkills.sort((a, b) => b.score - a.score);
+      setFilteredSkills(sortedSkills);
     } else {
-      setDisplayedSkills(
-        aboutSkills.filter(skill => skill.types.some(item => item === currFilter))
+      setFilteredSkills(
+        aboutSkills
+          .filter(skill => skill.types.some(item => item === currFilter))
+          .sort((a, b) => b.score - a.score)
       );
     }
-  }, [currFilter]);
+  }, [currFilter, searchInput]);
   return (
     <Tabs
       tabGroupId="About"
@@ -134,20 +189,23 @@ const AboutPage = ({ location }) => {
                 className={selectedTab === 'Developer' ? 'Visible' : 'Invisible'}
               >
                 <StyledParagraph>
-                  1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum
+                  Hey there! Thanks for checking out my site. I am a developer with over 3 years and
+                  thousands of hours of professional software development experience. I am driven
+                  and have a passion for solving difficult problems.
                 </StyledParagraph>
                 <StyledParagraph>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum
+                  I&apos;m a Software Engineer at{' '}
+                  <ExternalLink url="economicmodeling.com">Emsi</ExternalLink>, an economic modeling
+                  firm specializing in labor market analytics. I excel at interpersonal
+                  communication, and pride myself in being a leader, a problem solver, and
+                  self-motivator.
+                </StyledParagraph>
+                <StyledParagraph>
+                  I love DevOps, APIs, the design and implementation of algorithms, software design
+                  patterns (such as SOLID), and have a passion for clean, well-documented, and
+                  well-tested code. Feel free to{' '}
+                  <ExternalLink url="mailto:taylorrogers@outlook.com">contact me</ExternalLink>{' '}
+                  should you have any questions or comments.
                 </StyledParagraph>
 
                 <StyledHeader
@@ -158,18 +216,22 @@ const AboutPage = ({ location }) => {
                 >
                   Selected Skills and Proficiencies
                 </StyledHeader>
-                <Select
-                  ariaLabel="Select Grouping of Skills to Display"
-                  options={filterOptions}
-                  onChange={e => {
-                    setFilter(e.target.value);
-                    console.log('setting ', e.target.value);
-                  }}
-                  selection={currFilter}
-                  placeholder="Filter to a Grouping of Skills"
-                />
+                <Flex>
+                  <StyledSelect
+                    ariaLabel="Select Grouping of Skills to Display"
+                    options={filterOptions}
+                    onChange={e => setFilter(e.target.value)}
+                    selection={currFilter}
+                    placeholder="Filter to a Grouping of Skills"
+                  />
+                  <StyledInput
+                    placeholder="Search Skills"
+                    value={searchInput}
+                    setValue={e => setSearchInput(e)}
+                  />
+                </Flex>
                 <AnimatedBarChart
-                  data={displayedSkills}
+                  data={searchInput ? searchResults : filteredSkills}
                   maxPossible={100}
                   location={location}
                   shouldAnimate={selectedTab === 'Developer'}
@@ -180,20 +242,23 @@ const AboutPage = ({ location }) => {
                 className={selectedTab === 'Writer' ? 'Visible' : 'Invisible'}
               >
                 <StyledParagraph>
-                  2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum
+                  As a writer, I&apos;m a long&#8208;time hobby&#8208;ist who just enjoys it as a
+                  creative output for expressing myself. I am a serial note&#8208;taker and have
+                  taken to putting my notes about development online in the hopes that the notes I
+                  have written for myself can be useful to others. You can check out my{' '}
+                  <InternalLink to="/dev">Developer Notes and Tutorials</InternalLink> for more on
+                  that.
                 </StyledParagraph>
                 <StyledParagraph>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum
+                  For more creative things, I enjoy writing fantasy fiction as a hobby (you know,
+                  like a nerd), as well as some poetry and music when I make the time. I also write
+                  and world&#8208;build quite a bit regarding my own fantasy world, &quot;Greater
+                  Ecumene&quot;, some of which I hope to eventually put on this site. I&apos;ve also
+                  been known to write non-fiction personal essays from time&#8208;to&#8208;time.
+                  Selected ones can be found on the{' '}
+                  <InternalLink to="/essays">
+                    section of my site devoted to such things.
+                  </InternalLink>
                 </StyledParagraph>
               </TabContent>
               <TabContent
@@ -201,20 +266,23 @@ const AboutPage = ({ location }) => {
                 className={selectedTab === 'Person' ? 'Visible' : 'Invisible'}
               >
                 <StyledParagraph>
-                  3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum
+                  As a person, I am a great lover of the outdoors and life&#8208;long devotee to
+                  self&#8208;improvement. I have previously been a professional whitewater rafting
+                  guide, and have taken that passion through the rest of my life, including rowing
+                  my own boat down the Grand Canyon in 2018. When outside, I&apos;d love to be doing
+                  the above, as well as rock climbing, whitewater kayaking, cross&#8208;country
+                  skiing, trail running, surfing, sailing, etc.
                 </StyledParagraph>
                 <StyledParagraph>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum
+                  I&apos;m also a big believer in constant self improvement, both physically and
+                  mentally. I am always looking to grow and better myself as a person, so you can
+                  often find my trying something new and unfamiliar (and failing, often, in the
+                  process).
+                </StyledParagraph>
+                <StyledParagraph>
+                  I also happen to be married to the most amazing, beautiful, and talented woman in
+                  the entire world (proof available upon request), with whom I spend most of my free
+                  time, along with our two dogs Ricochet and Rosie.
                 </StyledParagraph>
               </TabContent>
             </div>
