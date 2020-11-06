944e7be875f1ef0439d182d72278cd164bd753c5
Taylor Rogers <taylor.rogers71@gmail.com>
2020-11-06 14:28:45 +0000

    add _app.js for global styles

---
+++ pages/_app.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+
+const GlobalStyles = ({ children }) => (
+  <>
+    {children}
+    <style jsx global>{`
+      html {
+        font-size: 62.5%;
+        height: 100%;
+      }
+
+      body {
+        height: 100%;
+        background: #07132e;
+        margin: 0;
+        padding: 0;
+        font-family: 'Roboto', 'Segoe UI', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+          'Droid Sans', 'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
+      #__next {
+        height: 100%;
+      }
+
+      code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+      }
+    `}</style>
+  </>
+);
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <GlobalStyles>
+          <Component {...pageProps} />
+        </GlobalStyles>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
