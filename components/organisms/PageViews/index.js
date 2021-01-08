83981e53d643e6fd9cdeb1d3dd3c724c13c2f45d
Taylor Rogers <taylor.rogers71@gmail.com>
2021-01-08 19:32:19 +0000

    Google analytics trials

--- components/organisms/PageViews/index.js
+++ components/organisms/PageViews/index.js
@@ -1,18 +1,20 @@
 import { useEffect } from 'react';
 import config from 'config/config';
-import { useLocation } from 'react-router-dom';
+import { useRouter } from 'next/router';
 
 // Google Analytics
 import ReactGA from 'react-ga';
 ReactGA.initialize(config.googleAnalyticsTrackingNumber);
 
 const PageViews = () => {
-  let location = useLocation();
+  let { asPath } = useRouter();
+  console.log('Google analytics: testing ', asPath, 'for localhost:3000');
   useEffect(() => {
-    if (window.location.hostname !== 'localhost') {
-      ReactGA.pageview(location.pathname + location.search);
+    if (window.location.hostname === 'localhost') {
+      ReactGA.pageview(asPath);
+      console.log('Google analytics: tested ', asPath, ' location:', window.location);
     }
-  }, [location]);
+  }, [asPath]);
   return null;
 };
 
