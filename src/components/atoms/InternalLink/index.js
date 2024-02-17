31ea63bac79494343bebf97c747554a556ba0d31
Taylor Rogers <taylor.rogers71@gmail.com>
2024-02-17 15:52:57 -0800

    rm all but infra

--- src/components/atoms/InternalLink/index.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-
-import { mutedBlue } from 'helpers/colors';
-
-export default styled(Link)`
-  color: ${mutedBlue};
-`;
