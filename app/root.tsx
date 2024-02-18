fc872512acffa5b530eeb8042c241a31dc023878
Taylor Rogers <taylor.rogers71@gmail.com>
2024-02-18 09:25:06 -0800

    add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -8,10 +8,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "~/tailwind.css";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: stylesheet },
 ];
+
 
 export default function App() {
   return (
