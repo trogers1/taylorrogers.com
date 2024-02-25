394eae93c1721ab81b4a16b0320049cdea470ede
Taylor Rogers <taylor.rogers71@gmail.com>
2024-02-24 16:05:47 -0800

    Link: href => to

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,16 +4,16 @@ import { Link } from '@remix-run/react';
 
 const Nav: FC = () => (
   <nav className="bg-gray-900 text-gray-50 py-4 px-4 flex items-center justify-between">
-    <Link className="font-medium" href="#">
+    <Link className="font-medium" to="#">
       Home
     </Link>
-    <Link className="font-medium" href="#">
+    <Link className="font-medium" to="#">
       About
     </Link>
-    <Link className="font-medium" href="#">
+    <Link className="font-medium" to="#">
       Services
     </Link>
-    <Link className="font-medium" href="#">
+    <Link className="font-medium" to="#">
       Contact
     </Link>
     <DarkModeToggle />
