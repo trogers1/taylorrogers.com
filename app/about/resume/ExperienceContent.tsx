ddb7df71f67835bc24d433ec71414db9ded00884
Taylor Rogers <taylor.rogers71@gmail.com>
2025-01-06 19:57:34 -0800

    Experience updated

--- app/about/resume/ExperienceContent.tsx
+++ app/about/resume/ExperienceContent.tsx
@@ -62,28 +62,37 @@ export const ExperienceContent: React.FC = () => {
             Initiated Agile and Scrum processes and refocused the team toward an
             ethos of testing and documentation.
           </li>{' '}
+        </ul>
+      </div>
+      <div>
+        <h3 className="text-xl font-semibold">
+          Software Engineer: React Developer
+        </h3>
+        <p className="text-gray-600 dark:text-gray-400">
+          <strong>Insights</strong> -- Dundee, UK | Nov 2020 - May 2021
+        </p>
+        <ul className="<ist-inside list-disc p-6">
           <li>
-            Led the development and launch of a brand-new, AI-based software in
-            less than 1 month.
-          </li>{' '}
-          <li>
-            Acted as both Engineering Manager and Technical Lead for two
-            separate teams and three separate projects, always fostering a
-            supportive and productive environment.
-          </li>{' '}
-          <li>
-            Increased CI/CD Pipeline efficiency, reducing deployment times by
-            80%.
+            Devised cross-team processes to increase collaboration and
+            efficiency.{' '}
           </li>
           <li>
-            Worked closely with stakeholders to shape the future of software,
-            leveraging experience to create achievable and measurable product
-            and platform goals.
+            Designed and built features in React and Typescript, such as the
+            entire application’s Frontend Permissions architecture.
           </li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="text-xl font-semibold">Full Stack Software Engineer</h3>
+        <p className="text-gray-600 dark:text-gray-400">
+          <strong>Emsi</strong> (later: Lightcast) -- Moscow, ID | Sep 2018 -
+          Sep 2020
+        </p>
+        <ul className="<ist-inside list-disc p-6">
           <li>
-            Cut operational costs for a SaaS offering by 60% within 3 months of
-            taking ownership of the project, while decreasing network response
-            times by 10-15%.
+            Engineered features for a student-centric SaaS product, leveraging
+            knowledge of React, Redux, AWS, ES6 Javascript, AWS Lambdas, Git,
+            APIs, and more.{' '}
           </li>
         </ul>
       </div>
