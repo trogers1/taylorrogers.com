c688736950f46210d3414664123d3ce63247c43b
Taylor Rogers <taylor.rogers71@gmail.com>
2025-01-06 21:25:11 -0800

    Fix contact section icon wrapping

--- app/about/resume/ContactSection.tsx
+++ app/about/resume/ContactSection.tsx
@@ -4,36 +4,42 @@ export const ContactSection: React.FC = () => (
   <CardHeader className="flex items-start justify-between bg-gray-800 p-6 text-white dark:bg-gray-700">
     <div className="w-full">
       <div className="mt-4 flex w-full flex-wrap items-center text-sm">
-        <MapPin className="mr-2 h-4 w-4" />
-        <span className="mr-4">Moscow, ID</span>
+        <div className="flex justify-center">
+          <MapPin className="mr-2 h-4 w-4" />
+          <span className="mr-4">Moscow, ID</span>
+        </div>
+        <div className="flex justify-center">
+          <Mail className="mr-2 h-4 w-4" />
+          <a
+            href="mailto:taylor.rea.rogers@gmail.com"
+            className="mr-4 hover:underline"
+          >
+            taylor.rea.rogers@gmail.com
+          </a>
+        </div>
+        <div className="flex justify-center">
+          <Github className="mr-2 h-4 w-4" />
+          <a
+            href="https://github.com/trogers1"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mr-4 hover:underline"
+          >
+            github.com/trogers1
+          </a>
+        </div>
 
-        <Mail className="mr-2 h-4 w-4" />
-        <a
-          href="mailto:taylor.rea.rogers@gmail.com"
-          className="mr-4 hover:underline"
-        >
-          taylor.rea.rogers@gmail.com
-        </a>
-
-        <Github className="mr-2 h-4 w-4" />
-        <a
-          href="https://github.com/trogers1"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mr-4 hover:underline"
-        >
-          github.com/trogers1
-        </a>
-
-        <Linkedin className="mr-2 h-4 w-4" />
-        <a
-          href="https://www.linkedin.com/in/taylor-r-rogers"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:underline"
-        >
-          linkedin.com/in/taylor-r-rogers
-        </a>
+        <div className="flex justify-center">
+          <Linkedin className="mr-2 h-4 w-4" />
+          <a
+            href="https://www.linkedin.com/in/taylor-r-rogers"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            linkedin.com/in/taylor-r-rogers
+          </a>
+        </div>
       </div>
     </div>
   </CardHeader>
