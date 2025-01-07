3a68b788cd4649a293811b062132ae3ed783daf3
Taylor Rogers <taylor.rogers71@gmail.com>
2025-01-06 19:51:38 -0800

    Education Content

--- app/about/resume/EducationContent.tsx
+++ app/about/resume/EducationContent.tsx
@@ -1,11 +1,16 @@
 export const EducationContent: React.FC = () => {
   return (
     <div>
-      <h3 className="text-xl font-semibold">
-        Minor in Computer Science BA History BA Spanish
-      </h3>
+      <h3 className="text-xl font-semibold">Minor, Computer Science</h3>
+      <h3 className="text-xl font-semibold">Minor, Natural Resources</h3>
       <p className="text-gray-600 dark:text-gray-400">
-        University of Idaho | Graduated: May 2014
+        University of Idaho | 2016 - 2018
+      </p>
+
+      <h3 className="text-xl font-semibold">BA, History</h3>
+      <h3 className="text-xl font-semibold">BA, Spanish</h3>
+      <p className="text-gray-600 dark:text-gray-400">
+        University of Idaho | 2010 - 2014{' '}
       </p>
     </div>
   );
